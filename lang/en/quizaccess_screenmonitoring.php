8fc7404a0a552fa01c771cc2365a095f8e2a7e65
Masud Bhuiyan <masud.bhuiya.m2@gmail.com>
2025-07-10 19:35:19 +0600

    Initialize project and create initial database schema

---
+++ lang/en/quizaccess_screenmonitoring.php
@@ -0,0 +1,8 @@
+<?php
+
+$string['pluginname'] = 'Screen Monitoring';
+$string['enablelabel'] = 'Enable screen monitoring';
+$string['enabled'] = 'Enabled';
+$string['disabled'] = 'Disabled';
+$string['screenshotdescription'] = 'Screen capture is enabled. You must share your screen to proceed.';
+$string['privacy:metadata'] = 'The screenmonitoring plugin does not store personal data directly.';
---
+++ version.php
@@ -0,0 +1,9 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'quizaccess_screenmonitoring';
+$plugin->version = 2025071100;
+$plugin->requires = 2022041900; 
+$plugin->maturity = MATURITY_ALPHA;
+$plugin->release = '0.1 (alpha)';
