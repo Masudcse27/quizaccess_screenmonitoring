dbeb1fde239168a86d6a1a34337ab838e8115ec8
Masud Bhuiyan <masud.bhuiya.m2@gmail.com>
2025-07-22 19:15:37 +0600

    Add "View Monitoring Report" button to quiz view page and capability for viewing reports

--- lang/en/quizaccess_screenmonitoring.php
+++ lang/en/quizaccess_screenmonitoring.php
@@ -4,4 +4,6 @@ $string['pluginname'] = 'Screen Monitoring';
 $string['enablelabel'] = 'Enable screen monitoring';
 $string['enabled'] = 'Enabled';
 $string['disabled'] = 'Disabled';
-$string['screenshotdescription'] = 'Screen capture is enabled. You must share your screen to proceed.';
+$string['screenshotdescription'] = 'Screen capture is enabled. You must share your entire screen to proceed.';
+$string['screenmonitoring:viewreport'] = 'View screen monitoring report';
+$string['monitoringreport'] = 'View Monitoring Report';
