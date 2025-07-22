36a9b8a80cd58e39f88bfad9a28cb7acd1cb3830
Masud Bhuiyan <masud.bhuiya.m2@gmail.com>
2025-07-22 17:45:51 +0600

    Create API for received screenshot from chrome extension and save on database

---
+++ db/services.php
@@ -0,0 +1,20 @@
+<?php
+$functions = [
+    'quizaccess_screenmonitoring_upload_image' => [
+        'classname'   => 'quizaccess_screenmonitoring_external',
+        'methodname'  => 'upload_image',
+        'classpath'   => 'mod/quiz/accessrule/screenmonitoring/externallib.php',
+        'description' => 'Uploads screenshot from Chrome extension',
+        'type'        => 'write',
+        'ajax'        => true,
+        'loginrequired' => false,
+    ],
+];
+
+$services = [
+    'Quiz Screen Monitoring API' => [
+        'functions' => ['quizaccess_screenmonitoring_upload_image'],
+        'restrictedusers' => 0,
+        'enabled' => 1,
+    ],
+];
